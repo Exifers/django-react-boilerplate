5cc374205ce502acce9e136ae7d03076679c2111
Emmanuel <meric.emmanuel@gmail.com>
2019-07-11 13:33:37 +0200

    [deployment]: added database support

--- home/settings.py
+++ home/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -67,7 +68,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    } if DEBUG else dj_database_url.config(conn_max_age=600)
 }
 
 
